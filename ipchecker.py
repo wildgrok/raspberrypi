6b89be76b1c25306e05fb25a0dc0ca969c6d8296
wildgrok <wildgrok@users.noreply.github.com>
2020-04-30 10:24:45 -0400

    Add files via upload
    added writelog

--- ipchecker.py
+++ ipchecker.py
@@ -2,8 +2,13 @@ smtpserver = "smtp.mail.yahoo.com"
 fromaddress = str('jbesad@yahoo.com')  
 toaddress = str('jbesad@yahoo.com')
 ipstorepath = r"/home/pi/Desktop/IPstore.txt"
+outfile = r"/home/pi/Desktop/ipchecker.out"
 username = str('jbesad@yahoo.com')  
-password = str('mzamaakovmkxqxeg')  
+password = str('mzamaakovmkxqxeg')
+
+def writelog(data):
+    with open((outfile), 'a+') as f:
+        f.write(data + '\n')
 
 def sendnewip(ipaddr, toaddress, fromaddress, password, smtpserver):
     #Module for sending emails
@@ -32,6 +37,11 @@ def updateipstore(ipaddr, ipstorepath):
     
 import requests, os
 
+if os.path.exists(outfile):
+    os.remove(outfile) 
+
+
+
 ipaddr = requests.get("https://api.ipify.org").text    
 
 if os.path.isfile(ipstorepath): #test if IP Store file exists
@@ -40,8 +50,12 @@ if os.path.isfile(ipstorepath): #test if IP Store file exists
         currentip.close()
         updateipstore(ipaddr, ipstorepath)
         sendnewip(ipaddr, toaddress, fromaddress, password, smtpserver) #Send email with new IP Address
-else: #Runs if IP Store file doesn't exist
-    updateipstore(ipaddr, ipstorepath) #Used to create IP store file
+        print("Your new IP is: " + str(ipaddr))
+        writelog("Your new IP is: " + str(ipaddr))
+    else: #Runs if IP Store file doesn't exist
+        updateipstore(ipaddr, ipstorepath) #Used to create IP store file
+        print("IP not changed: " + str(ipaddr))
+        writelog("IP not changed: " + str(ipaddr))
     
 
 
--- web_get_page.py
+++ web_get_page.py
@@ -1,11 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.7
+import sys
+import os
+#sys.path.append('/home/pi/.local/lib/python3.7/site-packages/pandas')
+#sys.path.append('/home/pi/.local/lib/python3.7/site-packages')
 import requests
 import pandas as pd
 import datetime
 
 workfolder = '/home/pi/Desktop/'
 
-print('Latest file - yesterday')
+def writelog(data):
+    with open((workfolder + 'web_get_page.out'), 'a+') as f:
+        f.write(data + '\n')
+        
+if os.path.exists(workfolder + 'web_get_page.out'):
+  os.remove(workfolder + 'web_get_page.out') 
+
+
+writelog(sys.version) 
+writelog('Latest file - yesterday')
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
 month = str(yesterday.month)
@@ -16,22 +29,29 @@ if len(month) < 2:
 if len(day) < 2:
     day = '0' + day
 yesterdayfile = month + '-' + day + '-' + year + '.csv'
-print(yesterdayfile)
+writelog(yesterdayfile)
 urlbase = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/'
 url = urlbase + yesterdayfile
-print(url)
+writelog(url)
 
-print('Beginning file download with requests')
+writelog('Beginning file download with requests')
 r = requests.get(url)
 #filename = url[-14:]
+
+if os.path.exists(workfolder + yesterdayfile):
+  os.remove(workfolder + yesterdayfile)
+  
+ 
+  
+#  /usr/bin/python3.7 /home/pi/Desktop/web_get_page.py > /home/pi/Desktop/web_get_page.out
 
 with open((workfolder + yesterdayfile), 'wb') as f:
     f.write(r.content)
 
 # Retrieve HTTP meta-data
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.encoding)
+writelog(str(r.status_code))
+writelog(r.headers['content-type'])
+writelog(r.encoding)
 
 df = pd.read_csv((workfolder +yesterdayfile), encoding = 'latin1')
 
@@ -39,14 +59,14 @@ df = pd.read_csv((workfolder +yesterdayfile), encoding = 'latin1')
 #   0               1               2       3   4           5           6       7           8     9         10            11                12                      13      14   15         16                17
 #Province_State Country_Region  Last_Update Lat Long_   Confirmed   Deaths  Recovered   Active  FIPS    Incident_Rate   People_Tested   People_Hospitalized Mortality_Rate  UID ISO3    Testing_Rate    Hospitalization_Rate
 #df = df.drop(df.columns[[0, 1, 3]], axis=1)  # df.columns is zero-based pd.Index
-# df = df.drop(df.columns[[                   1,             2,      3,   4,         5,                   7,          8,   9,          10,           11,             12,                              14,  15,        16,               17]], axis=1)
-# df = df[df.Province_State != 'Recovered']
+df = df.drop(df.columns[[                   1,                   3,   4,         5,                   7,          8,   9,          10,           11,             12,                              14,  15,        16,               17]], axis=1)
+df = df[df.Province_State != 'Recovered']
 
 
 # print(df)
-print ('these are the columns')
+writelog('these are the columns')
 for col in df.columns:
-    print(col)
+    writelog(col)
 
 html = df.to_html()
 #print(html)
