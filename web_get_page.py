b9c1dd4a7d4ff6af89b37dcb32a4f74089f5014d
wildgrok <wildgrok@users.noreply.github.com>
2020-05-21 21:56:38 -0400

    Add files via upload
    fixes 5/21/2020

--- web_get_page.py
+++ web_get_page.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.7
-#Last modified 5/12/2020
+#Last modified 5/21/2020
 import sys
 import os
 import requests
@@ -10,7 +10,6 @@ import datetime
 workfolder = '/home/pi/Desktop/'
 webfolder  = '/var/www/html/'
 urlbase = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/'
-
 
 def writelog(data):
     with open((workfolder + 'web_get_page.out'), 'a+') as f:
@@ -42,7 +41,6 @@ def get_data_to_csv(csvfile, urlbase):
     writelog(r.encoding)
     writelog(csvfile)
 
- 
 def get_data():    
     if os.path.exists(workfolder + 'web_get_page.out'):
         os.remove(workfolder + 'web_get_page.out') 
@@ -52,40 +50,60 @@ def get_data():
     writelog('Today:' + str(today))
     writelog('Latest file - yesterday:' + str(yesterday))
     weekago = today - datetime.timedelta(days=7)
-    writelog('File -week ago:' + str(weekago))  
+    #weekago = today - datetime.timedelta(days=14)
+    writelog('File -week ago:' + str(weekago))
     yesterdayfile = get_csv_filename(yesterday)
     weekagofile = get_csv_filename(weekago) 
     writelog(yesterdayfile)
     writelog(weekagofile)
       
     get_data_to_csv(yesterdayfile, urlbase)
-    
+
     #----------------------------------adding car deaths 2018------------------------------
+    # Province_State,Population_2018,Vehicle miles traveled (millions),Fatal crashes,Deaths_Car_2018,"Deaths per 100,000 population",Deaths per 100 million vehicle miles traveled
     carsdf = pd.read_csv((workfolder + 'car_accident_deaths_usa_2018.csv'), encoding = 'latin1')
     writelog('these are the columns for car deaths 2018')
     carsdf = carsdf.set_index('Province_State')
     for col in carsdf.columns:
         writelog(col)
     html = carsdf.to_html()
-    webpage = workfolder + 'car_accident_deaths_usa_2018.html'
+    webpage = webfolder + 'car_accident_deaths_usa_2018.html'
     with open(webpage, 'wt') as f:
         f.write(html)
     #----------------------------------end of adding car deaths 2018--------------------------
 
+    #--------------------------------adding all deaths 2018-----------------------------------
+    # Province_State,Births,Fertility_Rate,Deaths,Death_Rate
+    # file: deaths_usa_2018.csv
+    deaths2018df = pd.read_csv((workfolder + 'deaths_usa_2018.csv'), encoding = 'latin1')
+    writelog('these are the columns for all deaths 2018')
+    deaths2018df = deaths2018df.set_index('Province_State')
+    deaths2018df = deaths2018df.drop(['Births','Fertility_Rate'], axis=1)
+    deaths2018df['Population_2018'] = carsdf['Population_2018']
+    writelog('these are the columns - all deaths 2018')
+    for col in deaths2018df.columns:
+        writelog(col)
+    html2 = deaths2018df.to_html()
+    webpage2 = webfolder + 'deaths_usa_2018.html'
+    with open(webpage2, 'wt') as f:
+         f.write(html2)
+    #---------------------------------end of adding all deaths 2018---------------------------
+
+
+
     df = pd.read_csv((workfolder + yesterdayfile), encoding = 'latin1')
-    df = df.set_index('Province_State')   
-    if os.path.exists(workfolder + weekagofile):
-        df_previous = pd.read_csv((workfolder + weekagofile), encoding = 'latin1')
-    else:
-        writelog("Missing previous file " + weekagofile)
-        #temp fix to continue process 
-        df_previous = pd.read_csv((workfolder + yesterdayfile), encoding = 'latin1')
+    df = df.set_index('Province_State')
+
+    # for later: check for no file FileNotFoundError: [Errno 2] No such file or directory: '05-02-2020.csv'
+    if os.path.exists(workfolder + weekagofile) == False:
+        weekagofile = yesterdayfile
+
+    df_previous = pd.read_csv((workfolder + weekagofile), encoding = 'latin1')
     df_previous = df_previous.set_index('Province_State')
 
 
     #Province_State Country_Region  Last_Update Lat Long_   Confirmed   Deaths  Recovered   Active  FIPS    Incident_Rate   People_Tested   People_Hospitalized Mortality_Rate  UID ISO3    Testing_Rate    Hospitalization_Rate
     df = df.drop(['Country_Region','Lat', 'Long_','Confirmed','Recovered',   'Active',  'FIPS',    'Incident_Rate',   'People_Tested',   'People_Hospitalized',  'UID', 'ISO3',    'Testing_Rate',    'Hospitalization_Rate'], axis=1)
-
     writelog('these are the columns - current')
     for col in df.columns:
         writelog(col)
@@ -95,7 +113,6 @@ def get_data():
         f.write(html)
         
     df_previous = df_previous.drop(['Country_Region','Lat', 'Long_','Confirmed','Recovered',   'Active',  'FIPS',    'Incident_Rate',   'People_Tested',   'People_Hospitalized',  'UID', 'ISO3',    'Testing_Rate',    'Hospitalization_Rate'], axis=1)
-
     writelog('these are the columns - previous')
     for col in df_previous.columns:
         writelog(col)
@@ -104,19 +121,28 @@ def get_data():
     with open(webpage, 'wt') as f:        
         f.write(html)
 
+
+    #link to cars dataframe
     df_previous['Population_2018'] = carsdf['Population_2018']
     df_previous['Deaths_Car_2018'] = carsdf['Deaths_Car_2018']
+    df_previous['Mortality_Rate_Cars_2018'] = carsdf['Deaths per 100,000 population']
+    #link to deaths 2018 dataframe
+    df_previous['Deaths_All_2018'] = deaths2018df['Deaths']
+    # df_previous['Death_Rate_All_2018'] = deaths2018df['Death_Rate']
+    df_previous['Mortality_Rate_All_2018'] = deaths2018df['Death_Rate']
 
-    df_previous['Mortality_Rate After'] = df['Mortality_Rate'] #add the Price2 column from df2 to df1
-    df_previous['Deaths After'] = df['Deaths']                 #add the Price2 column from df2 to df1
-    df_previous['Deaths Diff'] = np.where(df_previous['Deaths'] == df['Deaths'], 0, df['Deaths'] - df_previous['Deaths']) #create new column in df1 for price diff
-    df_previous['Mortality Death Diff'] = np.where(df_previous['Mortality_Rate'] == df['Mortality_Rate'], 0, df['Mortality_Rate'] - df_previous['Mortality_Rate']) #create new column in df1 for price diff
+
+    #links to current week
+    df_previous['Deaths_After'] = df['Deaths']                 #add the Price2 column from df2 to df1
+    df_previous['Mortality_Rate_After'] = df['Mortality_Rate'] #add the Price2 column from df2 to df1
+    df_previous['Deaths_Diff_After'] = np.where(df_previous['Deaths'] == df['Deaths'], 0, df['Deaths'] - df_previous['Deaths']) #create new column in df1 for price diff
+    df_previous['Mortality_Rate_Diff'] = np.where(df_previous['Mortality_Rate'] == df['Mortality_Rate'], 0, df['Mortality_Rate'] - df_previous['Mortality_Rate']) #create new column in df1 for price diff
 
 
     #df_previous = df_previous.sort_values(by=['Deaths Diff'])
-    df_previous = df_previous.sort_values(by=['Mortality Death Diff'])
+    df_previous = df_previous.sort_values(by=['Mortality_Rate_Diff'])
 
-    writelog('these are the columns - added Mortality Rate After, Deaths After, MD Diff, Deaths Diff')
+    writelog('these are the columns - added Mortality Rate After, Deaths After, MD Diff, Deaths Diff, Deaths_2018, Death_Rate_2018')
     for col in df_previous.columns:
         writelog(col)
     html = df_previous.to_html()
@@ -133,3 +159,4 @@ def get_data():
 # Run program 
 if __name__ == '__main__':
     get_data()
+
