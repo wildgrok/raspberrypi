e14bed042f8de487364881692848162a19704897
wildgrok <wildgrok@users.noreply.github.com>
2020-11-22 09:11:06 -0500

    Add files via upload

---
+++ web_get_page.cmd
@@ -0,0 +1,14 @@
+date /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
+time /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
+
+cd C:\Users\python\PycharmProjects
+python web_get_page.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+python web_get_page2.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+python web_get_page_world.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+
+python plots_coronavirus.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+
+
+
+date /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
+time /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
