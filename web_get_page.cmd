182fc4c579483ece0e358538c658697410cb5413
wildgrok <wildgrok@users.noreply.github.com>
2021-06-26 12:58:25 -0400

    Add files via upload
    changed to single cmd

--- web_get_page.cmd
+++ web_get_page.cmd
@@ -1,14 +1,25 @@
-date /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
-time /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
+date /t >> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+time /t >> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
 
-cd C:\Users\python\PycharmProjects
-python web_get_page.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
-python web_get_page2.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
-python web_get_page_world.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+cd C:\Users\admin\Documents\coronavirus
 
-python plots_coronavirus.py >> C:\Users\python\PycharmProjects\web_get_page_load.out
+echo "Creating webpages with web_get_page3.py " 			>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+python web_get_page3.py 						>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+echo "Feeding mysql database with mysql_toolkit.py " 			>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+python mysql_toolkit.py 						> mysql_toolkit.out
+
+echo "Creating images for webpages with plots_coronavirus.py " 		>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+python plots_coronavirus.py 						>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+echo "Feeding besada.com pages and images and data files with WinSCP " 	>> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+
+"C:\Program Files (x86)\WinSCP\winscp.com" /ini=nul /script=C:\Users\admin\Documents\coronavirus\winscp_ftp_to_besadacom.txt
 
 
 
-date /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
-time /t >> C:\Users\python\PycharmProjects\web_get_page_load.out
+date /t >> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
+time /t >> C:\Users\admin\Documents\coronavirus\web_get_page_load.out
