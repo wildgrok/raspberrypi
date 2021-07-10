1b2f9ee7a6094669fcf6ffef409bf3a125d90cef
wildgrok <wildgrok@users.noreply.github.com>
2021-07-10 19:33:42 -0400

    Add files via upload
    PS C:\coronavirus> & C:/Python/python.exe c:/coronavirus/plots_coronavirus2.py
          Province_State Country_Region          Last_Update  Deaths
    0            Alabama             US  2020-04-24 03:33:00     202
    59           Alabama             US  2020-04-25 06:32:46     209
    118          Alabama             US  2020-04-26 02:32:45     209
    ...              ...            ...                  ...     ...
    24671        Wyoming             US  2021-07-08 04:31:15     751
    24729        Wyoming             US  2021-07-09 04:30:41     751
    24787        Wyoming             US  2021-07-10 04:31:17     751
    
    [24788 rows x 4 columns]
    List of columns
    Province_State
    Country_Region
    Last_Update
    Deaths
    ['Alabama' 'Alaska' 'American Samoa' 'Arizona' 'Arkansas' 'California'
     'Colorado' 'Connecticut' 'Delaware' 'Diamond Princess'
     'District of Columbia' 'Florida' 'Georgia' 'Grand Princess' 'Guam'
     'Hawaii' 'Idaho' 'Illinois' 'Indiana' 'Iowa' 'Kansas' 'Kentucky'
     'Louisiana' 'Maine' 'Maryland' 'Massachusetts' 'Michigan' 'Minnesota'
     'Mississippi' 'Missouri' 'Montana' 'Nebraska' 'Nevada' 'New Hampshire'
     'New Jersey' 'New Mexico' 'New York' 'North Carolina' 'North Dakota'
     'Northern Mariana Islands' 'Ohio' 'Oklahoma' 'Oregon' 'Pennsylvania'
     'Puerto Rico' 'Recovered' 'Rhode Island' 'South Carolina' 'South Dakota'
     'Tennessee' 'Texas' 'Utah' 'Vermont' 'Virgin Islands' 'Virginia'
     'Washington' 'West Virginia' 'Wisconsin' 'Wyoming']
    Alabama
    Wyoming

--- plots_coronavirus2.py
+++ plots_coronavirus2.py
@@ -1,6 +1,9 @@
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.area.html
 # created from plots.py 11/14/2020
 # last update:
+#7/10/2021
+#7/9/2021 version with no mysql
+#6/22/2021: version in dell laptop
 #12/3/2020 fix to remove axis to image
 #11/21/2020: created json files
 #11/21/2020: created state deaths files
@@ -10,11 +13,60 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import subprocess
 
 #globals-------------------------------------------------
-#statedeathsfolder = r'C:\Users\python\PycharmProjects\coronavirus\state_deaths'
-statedeathsfolder = '/home/pi/Documents/state_deaths'
+statedeathsfolder = r'C:\coronavirus\state_deaths'
+# statedeathsfolder = r'C:\Users\admin\Documents\coronavirus\state_deaths'
+database = r'C:\coronavirus\data_usa.csv'
+
+
+#create dataframe from dbfile
+pd.set_option('display.max_rows', 6)
+
+
+
+country = ['US']
+# df1 = pd.read_csv(database, encoding = 'latin1', thousands=',', usecols = ['Province_State','Country_Region','Last_Update','Deaths'])
+df1 = pd.read_csv(database, encoding = 'latin1', thousands=',')
+# df1 = df1.drop_duplicates
+# print(df1)
+
+df1=df1.sort_values(by=['Province_State', 'Last_Update'])
+print(df1)
+list_columns = df1.columns
+print('List of columns')
+# # print (list_columns)
+for x in list_columns:
+     print(x)
+
+# df1 = df1.astype({'Deaths': 'int32','Last_Update':'datetime64[ns]'}).dtypes
+# df1 = df1.set_index('Province_State')
+# df1 = df1['Country_Region'].isin(country)
+#list = df1['Province_State']
+# lst_states = df1.keys().unique().sort_values()
+lst_states = df1['Province_State'].unique()
+print(lst_states)
+print(lst_states[0])
+print(lst_states[-1])
+# cls
+# print(df1)
+# df1 = df1.diff
+# print(df1)
+
+
+
+# Make sure your data is sorted properly
+# df = df.sort_values(by=['group_var', 'value'])
+
+# only take diffs where next row is of the same group
+# df1['diffs'] = np.where(df1.Province_State == df1.Province_State.shift(1), df1.value.diff(), 0)
+# df1 = df1.sort_values(['Province_State', 'Last_Update'], inplace=True)
+# df1.sort_values(by  = ['Province_State', 'Last_Update'])
+# df1['diffs'] = df1.groupby('Province_State')['Deaths'].diff()
+# In order to return to the original order, you can the use
+
+# df.sort_index(inplace=True)
+
 #--------------------------------------------------------
 
 def get_state_chart(state):
@@ -32,57 +84,60 @@ def get_state_chart(state):
 
     statejpgfile = statedeathsfolder + '\\' + state + '.jpg'
     plt.savefig(statejpgfile)
+#================================================================
+#df1 = df1.astype({'Deaths': 'int32'}).dtypes
+
+# using apply method
+#df1['Deaths']= df1['Deaths'].apply(pd.to_numeric)
+
+# print(df1.drop_duplicates)
+# print(df1.columns.values)   
 
 
-
-
+#print(df1.diff())
 
 #list of csv files
 #csvfolder = r'C:\Users\python\PycharmProjects\coronavirus\csv2'
 
 
 
-
+#DONE, states list done in line lst_states = df1.keys().unique().sort_values()
 #get distinct list of states--------------------------------------------
-states = 'select distinct Province_State from coronavirus.data_usa2;'
-# sql = "SELECT * FROM coronavirus.data_usa2 where Province_State = 'Florida' order by 3 desc,1,2;"
-# cmd = '"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql" -udatareader -pdatareader -e "' + states
-#cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -udatareader -pdatareader -e "' + states
-cmd = 'mysql -udbuser -pdbuserpassword -e ' + "'" + states + "'"
-lst_states = os.popen(cmd).readlines()
-#print(lst_states)
+# states = 'select distinct Province_State from coronavirus.data_usa2;'
+# # sql = "SELECT * FROM coronavirus.data_usa2 where Province_State = 'Florida' order by 3 desc,1,2;"
+# # cmd = '"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql" -udatareader -pdatareader -e "' + states
+# # cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -udatareader -pdatareader -e "' + states
+# cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -uroot -pDinosaur1? -e "' + states
+# lst_states = os.popen(cmd).readlines()
 
 #------------------------------------------------------------------------
 # print(lst)
 
+
+
 #get txt--------------------------------------------------------------------
-for x in lst_states:
-    st = x.rstrip('\n')
-    if st == 'Florida':
-        
-        
-        print(st)
+# for x in lst_states:
+#     state = x.rstrip('\n')
+#     print(state)
 
-        #s = " SELECT a.Last_Update ,b.Deaths - a.Deaths as Deaths "
-#         s = " SELECT a.Last_Update ,b.Deaths - a.Deaths "
-#         s = s + "  FROM coronavirus.data_usa2 a "
-#         s = s + " join coronavirus.data_usa2 b on "
-#         s = s + " date(b.Last_Update) > date(a.Last_Update) and "
-#         s = s + " date(b.Last_Update) <= date(a.Last_Update) + 1 and "
-#         s = s + " b.Province_State = a.Province_State "
-#         s = s + " where a.Province_State = '" + state + "' "
-#         s = s + " and b.Deaths - a.Deaths >= 0;"
-#         #s = s + " order by 1"
+#     s = " SELECT a.Last_Update ,b.Deaths - a.Deaths as Deaths "
+#     s = s + "  FROM coronavirus.data_usa2 a "
+#     s = s + " join coronavirus.data_usa2 b on "
+#     s = s + " date(b.Last_Update) > date(a.Last_Update) and "
+#     s = s + " date(b.Last_Update) <= date(a.Last_Update) + 1 and "
+#     s = s + " b.Province_State = a.Province_State "
+#     s = s + " where a.Province_State = '" + state + "'"
+#     s = s + " and b.Deaths - a.Deaths >= 0 "
+#     s = s + " order by 1"
 
-        s = "SELECT a.Last_Update,a.Deaths FROM data_usa2 a WHERE a.Province_State = '" + st + "' order by a.Last_Update;"
-        # print(sql)
-        cmd = 'mysql -udbuser -pdbuserpassword coronavirus -e ' + "'" + s + "'" 
-        lst2 = os.popen(cmd)
-        # print(lst2[0:10])
-        statefile = statedeathsfolder + '/' + st + '.txt'
-        with open(statefile, 'w') as f:
-            f.writelines(lst2)
-            #f.write(lst2)
+#     # sql = "SELECT Last_Update,Deaths FROM coronavirus.data_usa2 where Province_State = '" + state + "' order by Last_Update;"
+#     # print(sql)
+#     cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -uroot -pDinosaur1? -e "' + s
+#     lst2 = os.popen(cmd).readlines()
+#     # print(lst2[0:10])
+#     statefile = statedeathsfolder + '\\' + state + '.txt'
+#     with open(statefile, 'w') as f:
+#         f.writelines(lst2)
 #-------------------------------------------------------------------------
 #         f.writelines('\n')
 #         # f.write(x+ '\n')
@@ -103,10 +158,10 @@ for x in lst_states:
 #     s = s + " where a.Province_State = '" + state + "'"
 #     s = s + " and b.Deaths - a.Deaths >= 0 "
 #     s = s + " order by a.Last_Update "
-# 
+
 #     #sql = "SELECT Last_Update,Deaths FROM coronavirus.data_usa2 where Province_State = '" + state + "' order by Last_Update;"
 #     # print(sql)
-#     cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -udatareader -pdatareader -e "' + s
+#     cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -uroot -pDinosaur1? -e "' + s
 #     lst2 = os.popen(cmd).readlines()
 #     # print(lst2[0:10])
 #     statefile = statedeathsfolder + '\\' + state + '.json'
@@ -116,10 +171,10 @@ for x in lst_states:
 
 
 
-#csv output----------------------------------------------------------------
+# #csv output----------------------------------------------------------------
 # for x in lst_states:
 #     state = x.rstrip('\n')
-# 
+
 #     s = " SELECT CONCAT_WS(',' ,a.Last_Update ,(b.Deaths - a.Deaths)) "
 #     s = s + "  FROM coronavirus.data_usa2 a "
 #     s = s + " join coronavirus.data_usa2 b on "
@@ -129,19 +184,19 @@ for x in lst_states:
 #     s = s + " where a.Province_State = '" + state + "'"
 #     s = s + " and b.Deaths - a.Deaths >= 0 "
 #     s = s + " order by 1"
-# 
-# 
+
+
 #     print(s)
-#     cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -udatareader -pdatareader  -e "' + s
+#     cmd = r'"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql" -uroot -pDinosaur1?  -e "' + s
 #     lst3 = os.popen(cmd).readlines()
 #     # print(lst2[0:10])
 #     statefile = statedeathsfolder + '\\' + state + '.csv'
 #     with open(statefile, 'w') as f:
 #         f.writelines('Last_Update,Deaths\n')
 #         f.writelines(lst3[1:])
-#------------------------------------------------------------------------
+# #------------------------------------------------------------------------
 
-#11/23/2020 jpg files---------------------------------------------------
+# #11/23/2020 jpg files---------------------------------------------------
 # for x in lst_states[1:]: #skipping Province_State line
 #     state = x.rstrip('\n')
 #     print('getting pic for ' + state)
