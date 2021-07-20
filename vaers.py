e8c092ebb4ae9ab57202958f76f6b17fcf353188
wildgrok <wildgrok@users.noreply.github.com>
2021-07-20 17:32:38 -0400

    Add files via upload

--- vaers.py
+++ vaers.py
@@ -12,11 +12,42 @@ today = datetime.date.today()
 csvfolder = 'C:/coronavirus/csv/'
 dbfile = 'C:/Users/admin/Downloads/2021VAERSData/2021VAERSData.csv'
 
+#------functions-----------------------------------
+# webpage is the file like index.html (full path)
+# table is the html content as table, coming from dataframe.to_csv
+# total is the count of the table items
+def make_html(webpage,table, total, daterange):
+    today = str(datetime.date.today())
+    s = '<html>'
+    s = s + '<body>'
+    s = '<h1>VAERS USA Data - updated from latest file from vaers.hhs.gov </h1>'
+    s = s + '<br><b>Date run: ' + today + ' - Total deaths ' + str(total) + '</b><br>'
+    s = s + '<br><b>' + daterange  + '</b><br>'
+    s = s + table
+    s = s + '<p>'
+    s = s  + '<a href="vaers_notes.html">Notes on data</a><p>'
+    s = s + '</body>'
+    s = s + '</html>'
+    # webpage = webfolder + 'index.html'
+    with open(webpage, "w", encoding="utf-8") as f:
+    # with open(webpage, 'wt') as f:
+        f.write(s)
+
+#------end of functions----------------------------
+
+
+
 #create dataframe from dbfile
 pd.set_option('display.max_rows', 2)
-df1 = pd.read_csv(dbfile, encoding = 'latin1', thousands=',', low_memory=False, usecols = ['VAERS_ID','RECVDATE','SYMPTOM_TEXT','STATE','AGE_YRS','DIED'])
+# df1 = pd.read_csv(dbfile, encoding = 'latin1', thousands=',', low_memory=False, usecols = ['VAERS_ID','RECVDATE','SYMPTOM_TEXT','STATE','AGE_YRS','DIED'])
+df1 = pd.read_csv(dbfile, encoding='latin1',thousands=',', low_memory=False, usecols = ['VAERS_ID','RECVDATE','SYMPTOM_TEXT','STATE','AGE_YRS','DIED'])
 columns = df1.columns
-# print('columns')
+print('Columns chosen:')
+for x in columns:
+    print(x)
+datesrange = 'Dates range:' + df1['RECVDATE'].min() + ' to ' + df1['RECVDATE'].max()
+print(datesrange)
+# print(df1['RECVDATE'].min())
 # print(columns)
 # Index(['VAERS_ID', 'RECVDATE', 'STATE', 'AGE_YRS', 'CAGE_YR', 'CAGE_MO', 'SEX',
 #        'RPT_DATE', 'SYMPTOM_TEXT', 'DIED', 'DATEDIED', 'L_THREAT', 'ER_VISIT',
@@ -26,26 +57,32 @@ columns = df1.columns
 #        'FORM_VERS', 'TODAYS_DATE', 'BIRTH_DEFECT', 'OFC_VISIT', 'ER_ED_VISIT',
 #        'ALLERGIES'],
 
-#print(df1)
-# df.loc[df['column_name'] == some_value]
-# only bring died with covid mentioned in SYMPTOM_TEXT
-# df1 = df1.loc[df1['DIED'] == 'Y']
-# df1 = df1.loc[df1['SYMPTOM_TEXT'] > '']
-# new_df = old_df[((old_df['C1'] > 0) & (old_df['C1'] < 20)) & ((old_df['C2'] > 0) & (old_df['C2'] < 20)) & ((old_df['C3'] > 0) & (old_df['C3'] < 20))]
 print('df1 no limitations count')
 print(df1.VAERS_ID.count())
+df1 = df1[ (df1['DIED'] == 'Y') & ( df1['SYMPTOM_TEXT'] > '' ) & df1['SYMPTOM_TEXT'].str.contains('covid', case = False)]
+print("df1 DIED and SYMPTOM_TEXT > '' and SYMPTON_TEXT contains covid count")
 
-
-l = ['covid','Covid','COVID']
-df1 = df1[ (df1['DIED'] == 'Y') & ( df1['SYMPTOM_TEXT'] > '' )]
-print("df1 DIED and SYMPTOM_TEXT > '' count")
-print(df1.VAERS_ID.count())
-l = ['covid','Covid','COVID']
-df1 = df1[df1['SYMPTOM_TEXT'].str.contains('covid', case = False)]
-print('df1 after string covid count')
-print(df1.VAERS_ID.count())
+deathscount = str(df1.VAERS_ID.count())
+print(deathscount)
 print('Saving file all columns')
 df1.to_csv('c:/coronavirus/vaers_covid_deaths.csv', index=False)
+
+print('Creating webpage')
+# make_html(webpage,table, total, daterange)
+# table = df1.to_html(na_rep='')
+table = df1.to_html(na_rep='', escape=False)
+wp = 'c:/coronavirus/vaers_covid_deaths.html'
+# with open(wp, "w", encoding="utf-8") as f:
+# # with open(wp, 'wt') as f:
+#         f.write(table)
+
+# print(table)
+
+webpage = 'c:/coronavirus/vaers.html'
+make_html(webpage,table, deathscount, datesrange)
+
+
+
 
 print('getting pic')
 df = df1.drop(['VAERS_ID','STATE','AGE_YRS','SYMPTOM_TEXT'], axis=1)
@@ -56,6 +93,15 @@ df["RECVDATE"] = pd.to_datetime(df["RECVDATE"])
 df.set_index('RECVDATE')
 
 df = df.groupby('RECVDATE').count()
+# df now:
+# RECVDATE,DIED
+# 2021-01-01,1
+# 2021-01-03,2
+# 2021-01-05,5
+# ...
+# 2021-07-07,20
+# 2021-07-08,7
+# 2021-07-09,12
 
 
 total_deaths = df.DIED.sum()
@@ -64,10 +110,7 @@ print(total_deaths)
 print('Saving file date and deaths only')
 # df.to_csv('c:/coronavirus/vaers_covid_deaths2.csv', index=False)
 df.to_csv('c:/coronavirus/vaers_covid_deaths2.csv')
-
-
-columns = df.columns
-
+# columns = df.columns
 ax = df.plot.area(stacked=False)
 # ax.set_axis_off()
 statejpgfile = 'c:/coronavirus/vaers.jpg'
