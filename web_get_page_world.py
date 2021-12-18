6846946137c3cb07af867a44e290890d1df3c55c
wildgrok <wildgrok@users.noreply.github.com>
2021-12-18 13:39:25 -0500

    Add files via upload

--- web_get_page_world.py
+++ web_get_page_world.py
@@ -27,7 +27,8 @@ def writelog(data):
         f.write(data + '\n')
 
 def make_webpage(df, webpage):
-    html = df.to_html(na_rep='')
+    # df = df.to_html(float_format='{:20,.2f}'.format)
+    html = df.to_html(na_rep='', float_format='{:20}'.format)
     webpage = webfolder + webpage
     with open(webpage, 'wt') as f:
         f.write(html)
@@ -65,13 +66,13 @@ def get_data_to_csv(csvfile, urlbase):
 def get_data():    
     if os.path.exists(workfolder + 'web_get_page_world.out'):
         os.remove(workfolder + 'web_get_page_world.out') 
-    writelog(sys.version)
+    # writelog(sys.version)
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
-    writelog('Today:' + str(today))
+    # writelog('Today:' + str(today))
     yesterdayfile = get_csv_filename(yesterday)
-    writelog('Latest file - yesterday:' + str(yesterday))
-    writelog(yesterdayfile)
+    # writelog('Latest file - yesterday:' + str(yesterday))
+    # writelog(yesterdayfile)
     get_data_to_csv(yesterdayfile, urlbase)
 
     df1 = pd.read_csv((workfolder + yesterdayfile), encoding = 'latin1')
@@ -84,12 +85,11 @@ def get_data():
     #ISO3 Testing_Rate Hospitalization_Rate
     # 
     df = df.drop(['Province_State', 'Lat', 'Long_', 'FIPS', 'Admin2', 'Confirmed', 'Recovered', 'Active','Combined_Key',	'Incident_Rate',	'Case_Fatality_Ratio'], axis=1)
-
     df = df.groupby(['Country_Region'])['Deaths'].sum().reset_index()
     df.rename(columns = {'Country_Region':'Province_State'}, inplace = True)
     # for col in df.columns:
     #     print(col)
-    df.set_index('Province_State')
+    #df.set_index('Province_State')
     make_webpage(df, 'daily_report_world.html')
 
     return df
@@ -101,46 +101,27 @@ def get_world_population():
     df1 = pd.read_csv(file, encoding='latin1',thousands=',', low_memory=False, usecols = columns)
     #rename
     df1.rename(columns = {'Country Name':'Province_State','2020':'Population'}, inplace = True)
-    df = df1.set_index('Province_State')
-    df.to_csv( workfolder + 'world_population_2020_dataframe.csv', index=True, encoding='utf-8')
-    for col in df.columns:
+    # df = df1.set_index('Province_State')
+    df1.to_csv( workfolder + 'world_population_2020_dataframe.csv', index=True, encoding='utf-8')
+    for col in df1.columns:
         print(col)
     #12/18/2021
-    make_webpage(df, 'world_population_2020.html')
-    return df
+    make_webpage(df1, 'world_population_2020.html')
+    return df1
 
 # Run program 
 if __name__ == '__main__':
-    # get_data()
-    # get_world_population()
+    # get countries deaths
     df = get_data()
-    # df = df.set_index('Province_State')
+    # get_world_population()
     df2 = get_world_population()
-    # df2 = df2.set_index('Province_State')
-    # df2['2020'] = df2['2020'].astype(float)
+    # add population
+    df = df.join(df2.set_index('Province_State'), on='Province_State')
 
-    # df['columnF'] = pd.Series(df1['columnF'])
-    # df['Population'] = pd.Series(df2['2020'])
-    # f_column = data2["columnF"]
-    # data1 = pd.concat([data1,f_column], axis = 1)
-    # data1
-    # f_column = df2["2020"]
-    # df = pd.concat([df,f_column], axis = 1)
-    
+    # print('---------df--------')
+    # print(df)
+    # print('----------df2-------')
+    # print(df2)
 
-
-    # df2 = df2.join(extracted_col)
-    # df = df.join(df2['2020'])
-    #print(df.describe)
-    print('---------df--------')
-    print(df)
-    print('----------df2-------')
-    print(df2)
-    # df.join(df2)
-    # # df['col1'] = df['col1'].astype(int)
-  
-    # print(df.describe)
-
-    # df.to_csv('c:/coronavirus/world_report_plus_population')
-
-    #adding world population to main data
+    # make web page
+    make_webpage(df, 'world_deaths_and_population.html')
